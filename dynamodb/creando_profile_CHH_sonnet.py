3a828464d4a856a585f5f91fde1d4a5f9c085956
JorgePrez <jorge_operez@yahoo.com>
2026-02-18 08:08:11 -0600

    ifnerencie profile

--- dynamodb/creando_profile_CHH_sonnet.py
+++ dynamodb/creando_profile_CHH_sonnet.py
@@ -4,14 +4,14 @@ import boto3
 client = boto3.client("bedrock", region_name="us-east-1")
 
 # Parámetros del nuevo perfil
-inference_profile_name = "CHH-Global-crossregion-claudesonnet45"
-description = "CHH usando Claude Sonnet 4.5 "
+inference_profile_name = "CHH-Global-crossregion-claudesonnet46"
+description = "CHH usando Claude Sonnet 4.6 "
 model_source = {
-    "copyFrom": "arn:aws:bedrock:us-east-1:552102268375:inference-profile/global.anthropic.claude-sonnet-4-5-20250929-v1:0"
+    "copyFrom": "arn:aws:bedrock:us-east-1:552102268375:inference-profile/global.anthropic.claude-sonnet-4-6"
 }
 tags = [
         {"key": "chatbot", "value": "CHH"},
-        {"key": "componente_chatbot", "value": "modelo_lenguaje_claude45sonnet"}
+        {"key": "componente_chatbot", "value": "modelo_lenguaje_claude46sonnet"}
 ]
 
 # Crear el perfil
@@ -26,3 +26,4 @@ response = client.create_inference_profile(
 print("Inference profile creado exitosamente:")
 print("ARN:", response["inferenceProfileArn"])
 
+# ARN: arn:aws:bedrock:us-east-1:552102268375:application-inference-profile/36bxaw3pfa0b  //mas reciente 18_02_2026
